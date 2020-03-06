12aa9d16928610a058b1bfe03122bf744be25b9b
Vladimir <vkonyukhoff@gmail.con>
2020-03-06 21:28:35 +0300

    this is my first commit

---
+++ test.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let persone = {
+    name: "Vladimir",
+    age: 28,
+    isDead: false
+}
+
+console.log(persone.isDead)
+
+let arr = [
+    'hellokitty.txt',
+    'apple.png',
+]
+
+for(let i = 0; i<arr.length;i++){
+    console.log(arr[i])
+}
